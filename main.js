b50fee7b154ee9567edd6bcd233ca028aaa36033
srcmilena <87768754+srcmilena@users.noreply.github.com>
2022-05-06 18:05:33 -0300

    day 2

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
+/* onScroll() { // Criação de função (on = ligar / ligar eventos)
+    console.log("mensagem");
+}*/
+
+//onScroll() // Execução da função 
+
+/*function onScroll() {
+    //console.log(scrollY) //console = objeto / log = funcionalidade do objeto (console)
+}*/
+
 function onScroll() {
-    
+    if (scrollY > 0) {
+        navigation.classList.add('scroll')
+    } else {
+        navigation.classList.remove('scroll')
+    }
+}
+
+function openMenu() {
+    document.body.classList('menu-expanded')
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu-expanded')
 }
